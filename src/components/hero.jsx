28bf9fb5d121cf3fd6dbb11e63f464f3fb6eae7f
bbizz <bbizz@DESKTOP-F1BIQ76>
2024-04-02 15:08:40 -0400

    added Navbar and home page

---
+++ src/components/hero.jsx
@@ -0,0 +1,89 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+export default function Hero() {
+  return (
+    <Box
+      id="hero"
+      sx={(theme) => ({
+        width: "100%",
+        backgroundImage:
+          theme.palette.mode === "light"
+            ? "linear-gradient(180deg, #CEE5FD, #FFF)"
+            : "linear-gradient(#02294F, #090E10)",
+        backgroundSize: "100% 20%",
+        backgroundRepeat: "no-repeat",
+      })}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
+          <Typography
+            component="h2"
+            variant="h2"
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignSelf: "center",
+              textAlign: "center",
+            }}
+          >
+            DMs &nbsp;
+            <Typography
+              component="span"
+              variant="h2"
+              sx={{
+                color: (theme) =>
+                  theme.palette.mode === "light"
+                    ? "primary.main"
+                    : "primary.light",
+              }}
+            >
+              Bag of Holding
+            </Typography>
+          </Typography>
+          <Typography variant="body1" textAlign="center" color="text.secondary">
+            Explore our Game Master dashboard, delivering high-quality Dungeon
+            Master solutions tailored to your needs. <br />
+            Elevate your experience with top-tier features and services.
+          </Typography>
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/Sign-Up"
+            >
+              Start now
+            </Button>
+          </Stack>
+          <Typography
+            variant="caption"
+            textAlign="center"
+            sx={{ opacity: 0.8 }}
+          >
+            Click &quot;Start now&quot; to create a free account and begin your
+            Adventure
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
